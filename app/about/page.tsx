bca9c1a9c3b348a3f3366a715130f6108023849c
mohamedeldesoukyh-cpu <mohamedeldesouky.h@gmail.com>
2026-05-20 06:10:43 +0300

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 
 export default function AboutPage() {
   return (
-    <main className="bg-[#f7f7f5] min-h-screen overflow-hidden relative">
+    <main className="bg-[#f7f7f5] min-h-screen overflow-hidden relative pt-[110px]">
 
       {/* GLOBAL VIDEO BACKGROUND */}
       <div className="fixed inset-0 z-0 opacity-[0.10]">
@@ -33,7 +33,7 @@ export default function AboutPage() {
         <Navigation />
 
         {/* ABOUT SECTION */}
-        <section className="pt-[80px] pb-[120px]">
+       <section className="pt-[40px] pb-[120px]">
 
   <div className="max-w-[1180px] mx-auto px-[80px]">
 
