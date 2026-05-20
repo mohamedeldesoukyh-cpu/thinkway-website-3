f47736a1172910510d2a5a172d8d91769f0db18e
mohamedeldesoukyh-cpu <mohamedeldesouky.h@gmail.com>
2026-05-20 06:22:36 +0300

    Update Navigation.tsx

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -72,7 +72,7 @@ export default function Navigation() {
             : "1px solid rgba(0,0,0,0.03)",
         }}
       >
-       <div className="container-custom pl-6 md:pl-12 flex items-center justify-between h-20">
+       <div className="w-full max-w-[1400px] mx-auto px-16 md:px-32 flex items-center justify-between h-20">
           
           {/* LOGO */}
           <a
