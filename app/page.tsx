fb03e345fc7465673802b0e255f8a8035277c98e
mohamedeldesoukyh-cpu <mohamedeldesouky.h@gmail.com>
2026-05-20 18:39:11 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import Clients from "@/components/Clients";
 import SOOH from "@/components/SOOH";
 import CreatorProgram from "@/components/CreatorProgram";
 import CampaignBrief from "@/components/CampaignBrief";
-import ContactUs from "@/components/ContactUs";
 import Footer from "@/components/Footer";
 
 
@@ -29,7 +28,6 @@ export default function Home() {
       <CreatorProgram />
       <CampaignBrief />
       <Clients />
-      <ContactUs />
       <Footer />
     </main>
   );
