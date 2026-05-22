d6f260c242da413ab0a8242ddf12ea4fdc64f9fa
Mohamed Eldesouky <mohamedeldesouky.h@gmail.com>
2026-05-22 10:14:08 +0300

    fix AIChat outside main overflow

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useEffect, useRef } from "react";
 import Navigation from "@/components/Navigation";
 import Hero from "@/components/Hero";
 import Marquee from "@/components/Marquee";
@@ -15,22 +14,23 @@ import CampaignBrief from "@/components/CampaignBrief";
 import Footer from "@/components/Footer";
 import AIChat from "@/components/AIChat";
 
-
 export default function Home() {
   return (
-    <main className="bg-white min-h-screen w-full overflow-x-hidden">
-      <Navigation />
-      <Hero />
-      <Marquee />
-      <Manifesto />
-      <Services />
-      <Creators />
-      <SOOH />
-      <CreatorProgram />
-      <CampaignBrief />
-      <Clients />
-      <Footer />
+    <>
+      <main className="bg-white min-h-screen w-full overflow-x-hidden">
+        <Navigation />
+        <Hero />
+        <Marquee />
+        <Manifesto />
+        <Services />
+        <Creators />
+        <SOOH />
+        <CreatorProgram />
+        <CampaignBrief />
+        <Clients />
+        <Footer />
+      </main>
       <AIChat />
-    </main>
+    </>
   );
 }
