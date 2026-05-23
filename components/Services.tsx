de4e7a80c16f39b2540d6f0cce1697e65017dea4
Mohamed Eldesouky <mohamedeldesouky.h@gmail.com>
2026-05-23 05:26:46 +0300

    improve services section layout with divider

--- components/Services.tsx
+++ components/Services.tsx
@@ -230,7 +230,7 @@ export default function Services() {
               initial={{ opacity: 0, x: -16 }}
               animate={inView ? { opacity: 1, x: 0 } : {}}
               transition={{ delay: 0.1, duration: 0.7 }}
-              className="mb-20"
+              className="mb-40"
             >
               
               <h3
